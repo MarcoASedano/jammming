6daacb62d38a75c941df05b494f428cc9315d382
Marco Sedano <marcoAsedano@gmail.com>
2019-03-28 21:06:59 -0700

    added reset.css file to ensure UI looks the same in all browsers

--- src/App.js
+++ src/App.js
@@ -5,21 +5,6 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
         <div>
           <h1>Ja<span class="highlight">mmm</span>ing</h1>
           <div class="App">
@@ -98,7 +83,6 @@ class App extends Component {
             </div>
           </div>
         </div>
-      </div>
     );
   }
 }
